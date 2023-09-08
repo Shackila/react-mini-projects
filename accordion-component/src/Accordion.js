094a72c92093ef7042965b5dac9f7df01ad93589
Shackila <mvd.shakila@gmail.com>
2023-09-08 12:39:39 +0330

    accordion item component updated.
    now only on box can be opened at the same time.

--- accordion-component/src/Accordion.js
+++ accordion-component/src/Accordion.js
@@ -1,10 +1,19 @@
 import AccordionItem from "./AccordionItem.js";
+import { useState } from "react";
 import "./styles.css";
 const Accordion = ({ data }) => {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={i} />
+        <AccordionItem
+          title={el.title}
+          text={el.text}
+          num={i}
+          key={i}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+        />
       ))}
     </div>
   );
--- accordion-component/src/AccordionItem.js
+++ accordion-component/src/AccordionItem.js
@@ -1,12 +1,12 @@
 import "./styles.css";
-import { useState } from "react";
-const AccordionItem = ({ num, text, title, icon }) => {
-  const [isOpen, setIsOpen] = useState(false);
+// import { useState } from "react";
+const AccordionItem = ({ num, text, title, curOpen, onOpen }) => {
+  const isOpen = num === curOpen;
+  const handleToggle = () => {
+    onOpen(isOpen ? null : num);
+  };
   return (
-    <div
-      className={`item ${!isOpen ? "open" : ""}`}
-      onClick={(e) => setIsOpen(!isOpen)}
-    >
+    <div className={`item ${!isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "+" : "-"}</p>
