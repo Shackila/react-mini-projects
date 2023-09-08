a4837cb77a6d3938f1b468c42c1ea86c310e69df
Shackila <mvd.shakila@gmail.com>
2023-09-08 12:51:27 +0330

    minor changes were applied to styles.css

--- accordion-component/src/AccordionItem.js
+++ accordion-component/src/AccordionItem.js
@@ -6,11 +6,11 @@ const AccordionItem = ({ num, text, title, curOpen, onOpen }) => {
     onOpen(isOpen ? null : num);
   };
   return (
-    <div className={`item ${!isOpen ? "open" : ""}`} onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "+" : "-"}</p>
-      <div>{isOpen && <div className="context-box">{text}</div>}</div>
+      {isOpen && <div className="context-box">{text}</div>}
     </div>
   );
 };
